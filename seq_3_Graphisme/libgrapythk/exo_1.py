69fef65c45c0f25cc596156367733414d826d85f
Laurent <2zbgnk@gmail.com>
2021-10-18 18:24:57 +0200

    fiche3/1 et fichiers associés

---
+++ seq_3_Graphisme/libgrapythk/exo_1.py
@@ -0,0 +1,15 @@
+#coding: utf-8
+from libgrapythk import *
+
+
+# ouverture de fenêtre
+g = ouvrirFenetre(800,600)
+
+# votre programme ICI
+
+# boucle à vide qui attend un clic
+while g.recupererClic() == None:
+    continue
+
+# fermeture fenêtre
+g.fermerFenetre()
